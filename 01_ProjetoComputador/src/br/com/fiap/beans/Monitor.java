b3232576f4961f510d940bf43adb58bf1e6a643e
rafadesiderio <rafael.d.programador@gmail.com>
2023-03-12 22:23:24 -0300

    Atributos de referencia e JOptionPane

---
+++ 01_ProjetoComputador/src/br/com/fiap/beans/Monitor.java
@@ -0,0 +1,49 @@
+package br.com.fiap.beans;
+
+public class Monitor {
+
+	// Modificador de acesso, tipo de dado e variável
+	private String marca;
+	private int tamanho;
+	private String tipo;
+	
+	
+	public Monitor () {
+		super();
+		
+	}
+	
+	public Monitor (String marca, int tamanho, String tipo ) {
+		super();
+		this.marca = marca;
+		this.tamanho = tamanho;
+		this.tipo = tipo;
+		
+	}
+
+	// Setters
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	// Getters
+	public String getMarca() {
+		return marca;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+}
